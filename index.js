1c90e9f4d1ac3f2f0c3033ba619f878cd3aa5994
Afrin <rukshanaafrinety797@gmail.com>
2026-05-19 16:36:22 +0600

    added to json data route

--- index.js
+++ index.js
@@ -11,65 +11,78 @@ app.get("/", (req, res) => {
     res.send("PawPal Server is Running");
 });
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const {
+    MongoClient,
+    ServerApiVersion,
+    ObjectId,
+} = require("mongodb");
 
-const uri = "mongodb+srv://rukshanaafrinety797_db_user:AeTXILcRbG5Xvqvv@cluster0.0ogo0ei.mongodb.net/?appName=Cluster0";
+const uri =
+    "mongodb+srv://rukshanaafrinety797_db_user:AeTXILcRbG5Xvqvv@cluster0.0ogo0ei.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
         strict: true,
         deprecationErrors: true,
-    }
+    },
 });
 
 async function run() {
-
     try {
 
-        const petsCollection = client.db("pawpalDB").collection("pets");
+        const petsCollection =
+            client.db("pawpalDB").collection("pets");
 
-
-        // POST API
+        // POST PET
 
         app.post("/pets", async (req, res) => {
 
             const newPet = req.body;
 
-            const result = await petsCollection.insertOne(newPet);
+            const result =
+                await petsCollection.insertOne(newPet);
 
             res.send(result);
 
         });
 
-
-        // GET API
+        // GET ALL PETS
 
         app.get("/pets", async (req, res) => {
 
-            const result = await petsCollection.find().toArray();
+            const result =
+                await petsCollection.find().toArray();
 
             res.send(result);
 
         });
 
+        // GET SINGLE PET
+
+        app.get("/pets/:id", async (req, res) => {
+
+            const id = req.params.id;
+
+            const result =
+                await petsCollection.findOne({
+                    _id: new ObjectId(id),
+                });
+
+            res.send(result);
+
+        });
 
         await client.connect();
 
         console.log("Connected to MongoDB!");
 
-    }
-
-    finally {
-
+    } finally {
     }
 }
 
 run().catch(console.dir);
 
-
 app.listen(port, () => {
-
     console.log(`Server running on port ${port}`);
-
 });
