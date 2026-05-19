6fe8c3e736287ff549ade3b654c1050ea5d341d0
Afrin <rukshanaafrinety797@gmail.com>
2026-05-19 14:45:26 +0600

    added to pet add setup

--- index.js
+++ index.js
@@ -11,10 +11,6 @@ app.get("/", (req, res) => {
     res.send("PawPal Server is Running");
 });
 
-app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
-});
-
 const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const uri = "mongodb+srv://rukshanaafrinety797_db_user:AeTXILcRbG5Xvqvv@cluster0.0ogo0ei.mongodb.net/?appName=Cluster0";
@@ -28,24 +24,52 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
+
     try {
 
         const petsCollection = client.db("pawpalDB").collection("pets");
 
+
+        // POST API
+
         app.post("/pets", async (req, res) => {
+
             const newPet = req.body;
 
             const result = await petsCollection.insertOne(newPet);
 
             res.send(result);
+
+        });
+
+
+        // GET API
+
+        app.get("/pets", async (req, res) => {
+
+            const result = await petsCollection.find().toArray();
+
+            res.send(result);
+
         });
 
 
         await client.connect();
+
         console.log("Connected to MongoDB!");
-    } finally {
+
+    }
+
+    finally {
 
     }
 }
 
 run().catch(console.dir);
+
+
+app.listen(port, () => {
+
+    console.log(`Server running on port ${port}`);
+
+});
