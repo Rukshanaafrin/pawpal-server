baf6deb69a25d5aba04f3da73c473922b7ea54e0
Afrin <rukshanaafrinety797@gmail.com>
2026-05-20 14:59:09 +0600

    added to API

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 const app = express();
 const port = 5000;
@@ -8,164 +9,136 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("PawPal Server is Running");
+  res.send("PawPal Server is Running");
 });
 
-const {
-    MongoClient,
-    ServerApiVersion,
-    ObjectId,
-} = require("mongodb");
-
 const uri =
-    "mongodb+srv://rukshanaafrinety797_db_user:AeTXILcRbG5Xvqvv@cluster0.0ogo0ei.mongodb.net/?appName=Cluster0";
+  "mongodb+srv://rukshanaafrinety797_db_user:AeTXILcRbG5Xvqvv@cluster0.0ogo0ei.mongodb.net/?appName=Cluster0";
 
 const client = new MongoClient(uri, {
-    serverApi: {
-        version: ServerApiVersion.v1,
-        strict: true,
-        deprecationErrors: true,
-    },
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
 });
 
 async function run() {
-    try {
+  try {
+    await client.connect();
 
-        await client.connect();
+    await client.db("admin").command({ ping: 1 });
 
-        const petsCollection =
-            client.db("pawpalDB").collection("pets");
+    console.log("Connected to MongoDB!");
 
-        const requestsCollection =
-            client.db("pawpalDB").collection("requests");
+    const petsCollection =
+      client.db("pawpalDB").collection("pets");
 
-        // POST PET
+    const requestsCollection =
+      client.db("pawpalDB").collection("requests");
 
-        app.post("/pets", async (req, res) => {
+    // ==========================
+    // PET APIs
+    // ==========================
 
-            const newPet = req.body;
+    app.post("/pets", async (req, res) => {
+      const newPet = req.body;
 
-            const result =
-                await petsCollection.insertOne(newPet);
+      const result =
+        await petsCollection.insertOne(newPet);
 
-            res.send(result);
+      res.send(result);
+    });
 
+    app.get("/pets", async (req, res) => {
+      const result =
+        await petsCollection.find().toArray();
+
+      res.send(result);
+    });
+
+    app.get("/pets/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result =
+        await petsCollection.findOne({
+          _id: new ObjectId(id),
         });
 
-        // GET ALL PETS
+      res.send(result);
+    });
 
-        app.get("/pets", async (req, res) => {
+    app.put("/pets/:id", async (req, res) => {
+      const id = req.params.id;
 
-            const result =
-                await petsCollection.find().toArray();
+      const updatedPet = req.body;
 
-            res.send(result);
+      const result =
+        await petsCollection.updateOne(
+          {
+            _id: new ObjectId(id),
+          },
+          {
+            $set: updatedPet,
+          }
+        );
 
+      res.send(result);
+    });
+
+    app.delete("/pets/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result =
+        await petsCollection.deleteOne({
+          _id: new ObjectId(id),
         });
 
-        // GET SINGLE PET
+      res.send(result);
+    });
 
-        app.get("/pets/:id", async (req, res) => {
+    // ==========================
+    // REQUEST APIs
+    // ==========================
 
-            const id = req.params.id;
+    app.post("/requests", async (req, res) => {
+      const requestData = req.body;
 
-            const result =
-                await petsCollection.findOne({
-                    _id: new ObjectId(id),
-                });
+      const result =
+        await requestsCollection.insertOne(
+          requestData
+        );
 
-            res.send(result);
+      res.send(result);
+    });
 
+    app.get("/requests", async (req, res) => {
+      const result =
+        await requestsCollection.find().toArray();
+
+      console.log("Requests:", result);
+
+      res.send(result);
+    });
+
+    app.delete("/requests/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const result =
+        await requestsCollection.deleteOne({
+          _id: new ObjectId(id),
         });
 
-        // UPDATE PET
+      res.send(result);
+    });
 
-        app.put("/pets/:id", async (req, res) => {
-
-            const id = req.params.id;
-
-            const updatedPet = req.body;
-
-            const result =
-                await petsCollection.updateOne(
-                    {
-                        _id: new ObjectId(id),
-                    },
-                    {
-                        $set: updatedPet,
-                    }
-                );
-
-            res.send(result);
-
-        });
-
-        // DELETE PET
-
-        app.delete("/pets/:id", async (req, res) => {
-
-            const id = req.params.id;
-
-            const result =
-                await petsCollection.deleteOne({
-                    _id: new ObjectId(id),
-                });
-
-            res.send(result);
-
-        });
-
-        // ==========================
-        // ADOPTION REQUEST APIs
-        // ==========================
-
-        // POST REQUEST
-
-        app.post("/requests", async (req, res) => {
-
-            const requestData = req.body;
-
-            const result =
-                await requestsCollection.insertOne(requestData);
-
-            res.send(result);
-
-        });
-
-        // GET ALL REQUESTS
-
-        app.get("/requests", async (req, res) => {
-
-            const result =
-                await requestsCollection.find().toArray();
-
-            res.send(result);
-
-        });
-
-        // DELETE REQUEST
-
-        app.delete("/requests/:id", async (req, res) => {
-
-            const id = req.params.id;
-
-            const result =
-                await requestsCollection.deleteOne({
-                    _id: new ObjectId(id),
-                });
-
-            res.send(result);
-
-        });
-
-        console.log("Connected to MongoDB!");
-
-    } finally {
-    }
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-run().catch(console.dir);
+run();
 
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
